8e98effee7348666601c3008554802eeef57595e
Дмитрий Баранов <demon.asgard@gmail.com>
2015-12-19 11:50:34 +0500

    Пожключен визуальный шаблон.

--- it_vesna/settings.py
+++ it_vesna/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'it_vesna.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'contest/../it_vesna/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- it_vesna/urls.py
+++ it_vesna/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from contest.views import *
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', home_page)
 ]
